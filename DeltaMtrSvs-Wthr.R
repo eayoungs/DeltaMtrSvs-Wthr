2eb5fa535cec1c20c63e768c2b9d4398782f19d1
Eric Youngson <eric@scneco.com>
2015-12-18 20:44:14 -0800

    Encapsulated script in functional block

--- DeltaMtrSvs-Wthr.R
+++ DeltaMtrSvs-Wthr.R
@@ -1,16 +1,17 @@
-library("lubridate")
 
-wundergrnd.FDL = read.csv("FDL_WI-KFLD_262014-2122015.csv", header = TRUE,
-                          stringsAsFactors = FALSE)
-
-mnthAvail = unique(month(wundergrnd.FDL$CST))
-
-for(i in 1:length(mnthAvail)) {
-  mnthMean = rbind(data.frame(mean(wundergrnd.FDL[month(wundergrnd.FDL$CST) == 
-             mnthAvail[i], "Mean.TemperatureF"]), row.names = 
-             month.abb[mnthAvail[i]]), mnthMean)
-
-  # mnthMeans = rbind(mnthMeans, mnthMean)
+mnthAveTemps <- function(){
+  library("lubridate")
+  
+  wundergrnd.FDL = read.csv("FDL_WI-KFLD_262014-2122015.csv", header = TRUE,
+                            stringsAsFactors = FALSE)
+  
+  mnthAvail = unique(month(wundergrnd.FDL$CST))
+  
+  for(i in 1:length(mnthAvail)) {
+    mnthMean = rbind(data.frame(mean(wundergrnd.FDL[month(wundergrnd.FDL$CST) == 
+               mnthAvail[i], "Mean.TemperatureF"]), row.names = 
+               month.abb[mnthAvail[i]]), mnthMean)
+  }
+  
+  write.csv(mnthMeans, file = "FDL_WI-KFLD_262014-2122015-MNTMEANS.csv")
 }
-
-write.csv(mnthMeans, file = "FDL_WI-KFLD_262014-2122015-MNTMEANS.csv")
