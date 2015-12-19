b0e3a63014252a03528047e784f087fc856ca0a4
Eric Youngson <eric@scneco.com>
2015-12-18 19:07:08 -0800

    Created a file, added command to calc monthly ave. from daily ave.s, loop through unique months in the set provided

--- DeltaMtrSvs-Wthr.R
+++ DeltaMtrSvs-Wthr.R
@@ -1,3 +1,9 @@
-wundergrnd.FDL = read.csv('FDL_WI-KFLD_262014-2122015.csv', header = TRUE, stringsAsFactors = FALSE)
 
-mean(wundergrnd.FDL[month(wundergrnd.FDL$CST) == 2,"Mean.TemperatureF"])
+wundergrnd.FDL = read.csv("FDL_WI-KFLD_262014-2122015.csv", header = TRUE,
+                          stringsAsFactors = FALSE)
+mnthAvail = unique(month(wundergrnd.FDL$CST))
+
+mnthAvailLen = length(mnthAvail)
+for(i in 1:mnthAvailLen) {
+  print(mean(wundergrnd.FDL[month(wundergrnd.FDL$CST) == i,"Mean.TemperatureF"]))
+}
